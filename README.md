0fafb7189be23b14e75ed93ea4662278c4921c4d
Christoffer Olofsson <christoffer.olofsson@goodsolutions.se>
2024-04-18 20:39:41 +0200

    Fixed heatmap.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-This is an extract from old resume like project based on PHP that has not been online for years. Go [here](https://chrisacrobat.github.io/FractalImagesGeneration/?corners=3&colorStep=100&cornerPlacement=0) to see it in action.
+This is an extract from old resume like project based on PHP that has not been online for years. Go [here](https://chrisacrobat.github.io/FractalImagesGeneration/?corners=3&cornerPlacement=0) to see it in action.
+
+|cornerPlacement | Function|
+|---|---|
+|0 | Even|
+|1 | Random pos|
+|2 | Random angle|
 
 Inspiration from [Chaos Game - Numberphile](https://youtu.be/kbKtFN71Lfs).
--- worker.js
+++ worker.js
@@ -1,48 +1,36 @@
 'use strict'
-var corners;
-var imageData;
-let colorStep;
-let frameSize;
+const frame = [];
+let max = 0;
 let x;
 let y;
+let loops = 1;
+const t = 1000/60;
 this.onmessage = messageEvent => {
-	this.onmessage = ()=>{
-		this.postMessage(imageData);
+	function update(){
+		let l = loops;
+		const start = Date.now();
+		while(l--){
+			const corner = corners[Math.floor(Math.random() * corners.length)];
+			x = Math.floor((x - corner.x)/2 + corner.x);
+			y = Math.floor((y - corner.y)/2 + corner.y);
+			const n = ++frame[y][x];
+			max = Math.max(max, n);
+		}
+		const duration = Date.now()-start;
+		loops -= Math.sign(duration - t);
+		loops = Math.max(1, loops)
+		setTimeout(update, 0);
 	}
-	corners = messageEvent.data.corners;
+	this.onmessage = ()=>{
+		this.postMessage({imageData: frame.flat(), max});
+	}
+	const corners = messageEvent.data.corners;
 	x = corners[0].x;
 	y = corners[0].y;
-	imageData = messageEvent.data.imageData;
-	colorStep = messageEvent.data.colorStep;
-	frameSize = messageEvent.data.frameSize;
-	updateNewPixel();
-}
-function updateNewPixel(){
-	for(var index = 0; index < corners.length; index++){
-		var corner = corners[Math.floor(Math.random() * corners.length)];
-		x = Math.floor((x - corner.x)/2 + corner.x);
-		y = Math.floor((y - corner.y)/2 + corner.y);
-		raisePixel(x, y);
+	const frameSize = messageEvent.data.frameSize;
+	for(let index = 0; index < frameSize; index++){
+		frame.push(new Array(frameSize).fill(0));
 	}
-	setTimeout(updateNewPixel, 0);
-}
-function raisePixel(x, y){
-	let i = (x + (y*frameSize))*4;
-	if(imageData.data[i+1] === 0){
-		imageData.data[i] += colorStep;
-	}
-	if(255 <= imageData.data[i]){
-		if(imageData.data[i+2] === 0){
-			imageData.data[i+1] += colorStep;
-		}
-		if(255 <= imageData.data[i+1]){
-			if(imageData.data[i] === 0){
-				imageData.data[i] -= colorStep;
-				imageData.data[i+2] += colorStep;
-			}
-		}
-		if(255 <= imageData.data[i+2]){
-			imageData.data[i+1] -= colorStep;
-		}
-	}
+	update();
+	onmessage();
 }
